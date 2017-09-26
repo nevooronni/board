4ad531d1be0eb56ffbe87556dfafe2677f3e1040
Neville Oronni <nevooronni@gmail.com>
2017-09-26 10:25:39 +0300

    added model data

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
+var questions = [{
+  id: 1,
+  name: "Veruca alvus",
+  content: "San Francisco"
+}, {
+  id: 2,
+  name: "eprupioupoi",
+  content: "wopripoweu"
+}, {
+  id: 3,
+  name: "wk;jrewj;",
+  content: ";lasfdkhaskjf"
+}];
+
 export default Ember.Route.extend({
+  model() {
+    return questions;
+  },
 });
