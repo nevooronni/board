690087dbb8b4bb06be405fec19b84aef346e6260
Neville Oronni <nevooronni@gmail.com>
2017-10-01 21:20:50 +0300

    added favorite question services

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -2,12 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isImageShowing: false,
+  favoriteQuestions: Ember.inject.service(),
+
   actions: {
     imageShow: function() {
       this.set('isImageShowing', true);
     },
     imageHide: function() {
       this.set('isImageShowing', false);
+    },
+    addToFavorite(question) {
+      this.get('favoriteQuestions').add(question);
     }
   }
 });
